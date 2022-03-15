7dbb243063a35a35ff620c7b95a203dce817ab9a
X <101196539+YidannXu@users.noreply.github.com>
2022-03-15 14:49:04 +0800

    modify variable.py

--- Practical5/variable.py
+++ Practical5/variable.py
@@ -3,10 +3,20 @@ b=4218520
 c=271
 d=b-c
 e=a-b
-print(d)
-print(e)
+w1="the difference between 2021 and 2020 is "
+w2="the difference between 2022 and 2021 is "
+print(w1+str(d))
+print(w2+str(e))
+if d>e:
+   print("d is greater than e")
+else:
+   print("e is greater than d")
+#outcome:d= 4218249
+       # e=15026781
+#answer:e is greater than d
+      # The rate of new cases is greater in 2021.
 
-outcome:d= 4218249
-        e=15026781
-answer:e is greater than d
-       The rate of new cases is greater in 2021.
+X="x"
+Y="y"
+W=X and Y
+print(W)
