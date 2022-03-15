070a922cdd1f9c493c1fb9b9c452bb72358fede7
X <101196539+YidannXu@users.noreply.github.com>
2022-03-15 15:04:08 +0800

    Update mystery_code.py

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer:maybe the code is to produce a random number from 1 to 99
+# Answer:produce 10 numbers of n from 1 to 100 randomly, print the tenth n
 # to get the number from the database
 # Import libraries
 # randint allows drawing a random number,
@@ -9,7 +9,7 @@ from random import randint
 # ceil takes the ceiling of a number, i.e. the next higher integer.
 # e.g. ceil(4.2)=5
 from math import ceil
-#set a variable progress make sure the code running properly
+#set a variable progress to make sure n obtained in the tenth time
 progress=0
 while progress<10:
 	progress+=1
