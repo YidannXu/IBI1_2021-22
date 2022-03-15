d3375a4fc3bf921d4e032147a37641e835548425
X <101196539+YidannXu@users.noreply.github.com>
2022-03-15 10:39:05 +0800

    edit variable.py

---
+++ Practical5/variable.py
@@ -0,0 +1,12 @@
+a=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+print(d)
+print(e)
+
+outcome:d= 4218249
+        e=15026781
+answer:e is greater than d
+       The rate of new cases is greater in 2021.
