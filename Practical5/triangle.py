48142306e879a039c025855125c62bec6fee9036
X <101196539+YidannXu@users.noreply.github.com>
2022-04-19 21:23:58 +0800

    edit triangle

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,10 +1,10 @@
-#initialize
+#d means the number of objects that can form an equilateral sequence and d is initialized to be 0. 
 d=0
-#use for loop to compute the first 10 values of sequence
+#Use for loop to compute the first 10 values of sequence. The nth triangle number is the number of dots or balls in a triangle with n dots on each side, which can be calculated as the sum of the n numbers from 1 to n.
 for i in range(1,11,1):
     d=d+i
     print (d) 
-#running result:
+#Here shows the running result:
 1
 3
 6
