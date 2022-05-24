76a098c38ab377f32621ccdf23212b0d6bd4c298
X <101196539+YidannXu@users.noreply.github.com>
2022-05-24 18:55:27 +0800

    edit covid.py

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -3,35 +3,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 #import the .csv file
-os.chdir("/home/chengzijiang/macbook/IBI1_2021-22/Practical7")
+os.chdir("/home/chengzijiang/macbook/IBI1_2021-22/Practical7") #marker can change the path here
 covid_data=pd.read_csv("full_data.csv")
 
 #show the first and third columns from rows 10-20
-covid_data.iloc[9:20,0:4:2]
+print(covid_data.iloc[9:20,0:3:2])
 
-#use a Boolean to show "total_cases" for all rows corresponding to Afghanistan and the similar function is from https://blog.csdn.net/weixin_35364187/article/details/113968240
-covid_data.loc[covid_data["location"]=="Afghanistan","total_cases"]
+#use a Boolean to show "total_cases" for all rows corresponding to Afghanistan
+my_columns = [False, False, False, False, True, False]
+my_rows = []
+for i in range(0,len(covid_data)):
+  if covid_data.loc[i,"location"] == "Afghanistan":
+     my_rows.append(True)
+  else:
+     my_rows.append(False)
+print(covid_data.loc[my_rows,my_columns])
 
 #compute the mean number of new cases and new deaths in China
 #access the specific row and column values by loc- and iloc-function
-china_new_cases=covid_data.loc[covid_data['location']=='China',['date','new_cases','new_deaths']]
+china_new_data=covid_data.loc[covid_data["location"]=="China",["date","new_cases","new_deaths"]]
 china_dates=china_new_data.iloc[:,0]
 #calculate the mean number and print them
-new_cases_mean=np.mean(china_new_cases["new_cases"])
-print(new_cases_mean)
-new_deaths_mean=np.mean(china_new_cases["new_deaths"])
-print(new_deaths_mean)
+new_cases_mean=np.mean(china_new_data["new_cases"])
+print("the mean number of new cases: ",new_cases_mean)
+new_deaths_mean=np.mean(china_new_data["new_deaths"])
+print("the mean number of new deaths: ",new_deaths_mean)
 
-#creat a boxplot of new cases in China worldwide
-china_data=covid_data.loc[covid_data['location']=='China',["date",'new_cases','new_deaths']]
-china_dates=china_data.loc[:,"date"]
-plt.boxplot(china_data.loc[:,"new_cases"],whis=1.5,patch_artist=True)
-plt.show()
-
-#creat a boxplot of new deaths in China worldwide
-china_data=covid_data.loc[covid_data['location']=='China',["date",'new_cases','new_deaths']]
-china_dates=china_data.loc[:,"date"]
-plt.boxplot(china_data.loc[:,"new_deaths"],whis=1.5,patch_artist=True)
+#creat a boxplot of new cases and new deaths in China worldwide
+x1=china_new_data["new_cases"]
+x2=china_new_data["new_deaths"]
+plt.boxplot([x1,x2],labels=["new_cases","new_deaths"])
+plt.title("the boxplot of new cases and new deaths in China worldwide")
+plt.ylabel("number")
 plt.show()
 
 #plot both new cases and new deaths in China over time
