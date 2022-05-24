1ef55f9aebf280319f05a745dbdacc12ca0a1b85
X <101196539+YidannXu@users.noreply.github.com>
2022-05-24 20:03:16 +0800

    move files from practical8 to practical9

---
+++ Practical9/count_cut_genes.py
@@ -0,0 +1,25 @@
+import re
+#allow the users to input a filename as the new fasta file.
+#Please note that this can only be done in python3 version
+new_fasta_file = input("input a new filename:")
+file = open('/Users/macbook/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa') #markers can change the path here
+result = open(new_fasta_file,"w")
+DNA = file.read()
+DNA = "".join(DNA.split("\n"))
+DNA = DNA.lstrip(">")
+genes = re.split(r">",DNA)
+output = " "
+#show the name of the gene,the number of cuts made in this gene by EcoRI and the DNA sequence
+for i in range(len(genes)):
+              if re.search(r"GAATTC",genes[i]):
+                  name = str(re.findall(r"gene:(.+?)\s",genes[i]))
+                  name = name.strip("['']")
+                  a = str(re.findall(r"](.+)",genes[i]))
+                  a = a.strip("['']")
+                  cuts_number = a.count("GAATTC")+1
+                  output = output+">"+name+"\n"+"the number of fragments is"+" "+str(cuts_number)+"\n"+a+"\n"
+result.write(output)
+file.close()
+result.close()
+#Through this codes, I have created an fasta file whose name is "new filename named by users.fa" in practical8.
+
